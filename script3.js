ff9c38ae2f4ff6c01329534bb06903b94f890234
kozlov721isip <145563641+kozlov721isip@users.noreply.github.com>
2023-09-21 12:45:38 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,27 @@
+"use strict";
+let r = 10;
+let PI  = 3.14;
+let s = 2 * r * PI;
+alert(s); //площадь круга
+
+let a = 5;
+let s1 = a ** 2;
+alert(s1); //площадь квадрата
+
+let a1 = 6;
+let b = 3;
+let s3 = a1  * b;
+alert(s3); прямоугольника
+
+let a2 = 12;
+let b2 = 6;
+let p = (a2 + b2) * 2;
+alert(p); прямоугольника
+
+let tC = 36;
+let F = 9/5 * tC + 32;
+alert(F);  
+
+let tf = 67;
+let C = (tf - 32) * 5/9;
+alert(C);
